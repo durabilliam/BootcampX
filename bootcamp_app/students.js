5f35008b9359b2be03fda437a9cd407d9f946966
William Perry <durabilliam@gamil.com>
2021-04-28 18:08:27 -0230

    sql injection

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,5 +1,5 @@
 const { Pool } = require('pg');
-let argument = process.argv.slice(2);
+let argument = process.argv;
 
 const pool = new Pool({
   user: 'vagrant',
@@ -8,13 +8,20 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
+
+pool.query(queryString, values);
+
+const queryString = `
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${argument[0]}%'
-LIMIT ${argument[1] || 5};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,5 +1,5 @@
 const { Pool } = require('pg');
-let argument = process.argv.slice(2);
+let argument = process.argv
 
 const pool = new Pool({
   user: 'vagrant',
@@ -8,16 +8,21 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const cohort = process.argv[2] || 'JUL02';
+const values =[`%${cohort}%`];
+
+pool.query(queryString, values);
+
+const queryString = `
 SELECT DISTINCT teachers.name AS teacher, 
                 cohorts.name AS cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${argument[0] || 'JUL02'}'
+WHERE cohorts.name = $1
 ORDER BY teacher;
-`)
+`
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.cohort}: ${user.teacher}`);
